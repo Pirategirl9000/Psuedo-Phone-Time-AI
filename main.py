b4438f1be6f00a189c77393a728b8360f1b56cb6
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2024-03-19 15:30:52 +0000

    //

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
-import tensorflow as tf
+import tensorflow
 import pandas as pd
 import numpy as np
 import keras
 from sklearn.model_selection import train_test_split
-import time 
-import os
+import time
 
-dataset = pd.readCSV('Phone_time_data.csv')
+dataset = pd.read_csv('Phone_time_data.csv')
 
 x = dataset.drop(columns=["age"])
 y = dataset["age"]
 
-x_train, x_test, y_train, y_test = train_test_split(dataset, test_size = 0.2, train_size = 0.8)
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 
 model = keras.models.Sequential()
 
-model.add(keras.layers.Dense(256, x.shape[1:], activation = 'relu', max_value = 24.0))
-model.add(keras.layers.Dense(256, activation = 'relu', max_value = 24.0))
-model.add(keras.layers.Dense(1, activation = 'relu', max_value = 100.0))
+model.add(keras.layers.Dense(256, input_shape = x.shape[1:], activation = 'linear'))
+model.add(keras.layers.Dense(256, activation = 'linear'))
+model.add(keras.layers.Dense(1, activation = 'linear'))
 
+model.compile(optimizer = 'adam', loss = "mean_absolute_error", metrics = ["accuracy"])
 model.fit(x_train, y_train, epochs = 500)
 
-model.compile(optimizer = 'adam', loss = "binary_crossentropy", metrics = ["accuracy"])
-
 time.sleep(3)
-os.system('cls')
 
 print(model.predict(np.array([[1,3,0,0,1,0]])))
 
